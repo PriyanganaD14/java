db1f6f676663d5a36c93b602a47c157286f9ee09
PriyanganaD14 <56598929+PriyanganaD14@users.noreply.github.com>
2020-03-01 23:02:59 +0530

    Add files via upload

---
+++ semi.java
@@ -0,0 +1,11 @@
+class Semi
+{
+
+    public static void main(String args[])
+    {
+    String str=";";
+    //char ch1=str.charAt(0);
+    //System.out.println(ch1);
+    System.out.println(str.charAt(0));
+}
+}
