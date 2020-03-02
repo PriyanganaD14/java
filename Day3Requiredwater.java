19c2645ea0c683d36dff02a23f46e173d9f92ba0
PriyanganaD14 <56598929+PriyanganaD14@users.noreply.github.com>
2020-03-02 23:52:40 +0530

    Add files via upload

---
+++ Day3Requiredwater.java
@@ -0,0 +1,11 @@
+class Water
+{
+    public static void main(String args[])
+    {
+    int bigcup=500;
+    int smallcup=300;
+    int requiredWater=bigcup-smallcup;
+    System.out.println("Required exact water = ");
+    System.out.println(requiredWater);
+    }
+}
