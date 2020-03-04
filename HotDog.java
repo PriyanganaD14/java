77280a82ac21e0bc1e84c98cd132a4f8fca27d5f
PriyanganaD14 <56598929+PriyanganaD14@users.noreply.github.com>
2020-03-04 23:34:59 +0530

    Add files via upload

---
+++ HotDog.java
@@ -0,0 +1,18 @@
+class Jack
+{
+    public static void main(String args[])
+    {
+        int totalHotDog=400;
+        int packetContained=8;
+        int totalPacket=0;
+        int temp=totalHotDog+packetContained;  //without using "\" or "%" operator.
+        while(temp>packetContained)
+        {
+            temp=temp-packetContained;
+            totalPacket++;
+        }
+        {
+            System.out.println("Total Packet did he buy : "+totalPacket);
+        }
+    }
+}
