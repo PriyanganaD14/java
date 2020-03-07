fcb0630e7a94c9a7f98e275443a79411d66963ca
PriyanganaD14 <56598929+PriyanganaD14@users.noreply.github.com>
2020-03-07 23:57:15 +0530

    Add files via upload

---
+++ Nurse.java
@@ -0,0 +1,14 @@
+class Nurse
+{
+    public static void main(String args[])
+    {
+        int nHoward=8;
+        int nPease=10;
+        int nCampbell=9;
+        int nGrace=8;
+        int nMcCarthy=7;
+        int nMurphy=12;
+        float avgHours=(8+10+9+8+7+12)/6;
+        System.out.println("The average hours :"+avgHours);
+    }
+}
