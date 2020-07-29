14d77b18d11a80006d10f306ca423fac7ac9ca7e
Priyangana Das <56598929+PriyanganaD14@users.noreply.github.com>
2020-07-29 11:13:49 -0700

    Add files via upload

---
+++ SortedArrayOrNotNaiveMethod.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+class SortedArrayOrNotNaiveMethod {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+         
+        System.out.println("Enter the size of the array :");
+        int sizeOfArray=sc.nextInt();
+
+        System.out.println("Enter the array :");
+        int arr[]=new int[sizeOfArray];
+        for(int i=0;i<sizeOfArray;i++){
+            int x=sc.nextInt();
+            arr[i]=x;
+        }
+
+        System.out.println("The array is sorted ?");
+        System.out.println(isSorted(arr));
+    }
+
+    public static boolean isSorted(int ar[]){
+
+        for(int i=0;i<ar.length;i++){
+  
+            for(int j=i+1;j<ar.length;j++){
+
+                if(ar[j]<ar[i]){
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+
+    
+}
